fab03427fc4870c974abc3d3461e04bba01061b9
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2024-01-11 11:22:54 +0300

    this is the second assignment

--- odev2.sql
+++ odev2.sql
@@ -10,7 +10,7 @@
 
 --film tablosunda bulunan tüm sütunlardaki verileri 
 --rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
---Kolay Gelsin.
+--Kolay Gelsin sizlere.
 
 ----------------------------------------------------
 
